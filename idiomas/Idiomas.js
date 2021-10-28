e86e3922cfb92cb8261c19fe03cb92d677cc5fef
Alvin <72828081+MisterAlvin@users.noreply.github.com>
2021-10-28 15:41:19 -0300

    Create Idiomas.js

---
+++ idiomas/Idiomas.js
@@ -0,0 +1,53 @@
+module.exports = {
+  // Portuguese - Português
+  "pt": {
+    cmds: {
+      avatar: {
+        titleEmbed: "Avatar de ",
+        click: ["Clique aqui ", "para baixar!"]
+            },
+       ping: {
+        descriptionEmbed: "Meu ping está a:",
+      },
+      botinfo: {
+        titleEmbed: "Sobre mim 👑",
+        description: ["Olá, me chamo **Sun** sua possível amiga que te ajudará em tudo no seu servidor.\n\nAqui está algumas informações abaixo.", "Estou em", "servidores com", "usuários com", "emojis.\n\n", "Meu discord"]
+      },
+      language: {
+descriptionEmbed: ["You changed my language to:"],
+description: ["Você precisa digitar um dos **2** idiomas: \`pt\` ou \`en\`."],
+esta: ["Este idioma já está no servidor."]
+      },
+      config: {
+        titleEmbed: "Configurações de ",
+        description: ["Linguagem do servidor: "]
+            }
+    }
+  },
+
+  // English - Inglês
+  "en": {
+    cmds: {
+      avatar: {
+        titleEmbed: "Avatar of ",
+        click: ["Click here ", "to download!"]
+      },
+       ping: {
+        descriptionEmbed: "My ping is:",
+      },
+      botinfo: {
+        titleEmbed: "About me 👑",
+        description: ["Hi, my name is Sun your possible friend who will help you with everything on your server.\n\nHere is some information below.", "I'm on", "servers with", "users with", "emojis.\n\n", "My server"]
+},
+language: {
+descriptionEmbed: ["Você alterou o meu idioma neste servidor para:"],
+description: ["You need to enter one of **2** languages: \`pt\` or \`en\`."],
+esta: ["This language is already on the server."]
+      },
+      config: {
+        titleEmbed: "Settings of ",
+        description: ["Server language: "]
+            }
+    }
+  }
+}
